bbaf65340652f4dc325e74b1505194501e717e82
katiakniazhevich <55781001+katiakniazhevich@users.noreply.github.com>
2021-03-15 22:02:46 +0300

    feat: add info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) { //проходим циклом по каждому элементу многомерного массива bracketsConfig
+      let symbolsOfSubArray = bracketsConfig[i].join('');//в переменную определяем объединенные элементы субмассива
+      if (str.includes(symbolsOfSubArray)) {//проверяем условие, если первый аргумент строка включает в себя парные символы
+          str = str.replace(symbolsOfSubArray, '');//заменяем совпадение на пустую строку
+          i = - 1;
+       }
+     }
+      return str.length === 0;
 }
